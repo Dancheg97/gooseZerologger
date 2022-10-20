3a46f9435fcada0cb30fcb492eba89eb22a82c24
Danila Fominykh <dfominykh@c2g.local>
2022-10-20 12:10:52 +0300

    final fix in logging format

--- adapter/logger.go
+++ adapter/logger.go
@@ -34,7 +34,7 @@ func (s *adapter) Println(v ...interface{}) {
 
 func (s *adapter) Printf(format string, v ...interface{}) {
 	if strings.HasSuffix(format, "\n") {
-		format = format[:len(format)-2]
+		format = format[:len(format)-1]
 	}
 	s.logger.Info().Msg(fmt.Sprintf(format, v...))
 }
